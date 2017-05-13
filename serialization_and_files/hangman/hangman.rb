0b414e153be0c9f7ada6ace34e3c7991874c1828
Clayton <cjsweeten101@gmail.com>
2017-05-12 20:46:52 -0600

    Hangman finished, file saving/loading works

--- serialization_and_files/hangman/hangman.rb
+++ serialization_and_files/hangman/hangman.rb
@@ -1,12 +1,12 @@
 class Hangman
-	attr_reader :word
+	attr_reader :word, :hint, :correct, :incorrect 
   
   def initialize(file, shortest_word=5, longest_word=12)
   	@dictionary = read_dictionary(file, shortest_word, longest_word)
-  	@word = choose_word
-  end
-
-  def game_loop
+  	@word = choose_word.strip.downcase
+  	@hint = ''.rjust(@word.length, '-')
+  	@correct = []
+  	@incorrect = []
   end
 
   def read_dictionary(file, shortest_word, longest_word)
@@ -14,7 +14,27 @@ class Hangman
   end
 
   def choose_word
-  	@dictionary[rand(0..@dictionary.length)]
+  	word = @dictionary[rand(0..@dictionary.length)]
+  	@dictionary = ''
+  	word
   end
 
+  def game_over?
+  	@hint == @word
+  end
+
+  def make_guess(letter)
+  	if @word.split('').include? letter
+  		@correct << letter
+  	else
+  		@incorrect << letter
+  	end
+  	change_hint
+  end
+
+  def change_hint
+  	@word.split('').each_with_index do |letter, i|
+  	  @hint[i] = letter if @correct.include? letter
+  	end
+  end
 end
--- serialization_and_files/hangman/play_hangman.rb
+++ serialization_and_files/hangman/play_hangman.rb
@@ -1,10 +1,11 @@
 class PlayerInterface
+  require 'yaml'
+  require_relative 'hangman'
 
   def initialize
   	display_greeting
+  	@hangman = ''
   end
-
-  require_relative 'hangman'
 
   def display_greeting
   	puts "Welcome!  Please Choose an option below!"
@@ -18,9 +19,14 @@ class PlayerInterface
     result = 0
     count = 0
     until result =~ expected_result do
-      puts "Sorry? I Didn't quite catch that." if count > 0 
-      result = gets.chomp
+      puts "Try again. . ." if count > 0 
+      result = gets.chomp.downcase
       count += 1
+   	  if result == 'quit'
+   	  	exit
+   	  elsif result == 'save'
+   	  	save_game
+   	  end
     end
     result
   end
@@ -39,15 +45,67 @@ class PlayerInterface
 
   def start_new_game
   	puts "Starting new game!"
-
-  	game = Hangman.new('5desk.txt')
-  	puts game.word
+  	puts "At anytime type 'quit' to quit, or 'save' to save this game for later"
+  	@hangman = Hangman.new('5desk.txt')
+  	game_loop
   end
 
   def load_old_game
-
+  	saves = []
+  	Dir.foreach('saves/') do |item|
+  	next if item == '.' or item == '..'
+  	saves << item.strip
+	end
+	puts "Saved Games: #{saves.join(' ')}"
+	choice = get_input(array_to_regex(saves))
+	@hangman = YAML::load_file("saves/#{choice}")
+	game_loop
   end
 
+  def game_loop
+    puts @hangman.word
+    turns = 10
+    until turns == 0 do
+      puts "Hint: #{@hangman.hint} | Incorrect Letters: #{@hangman.incorrect.join(',')} | Turns Left: #{turns}" 
+      victory?
+
+      unguessed_chars = array_to_regex(('a'..'z').to_a - (@hangman.correct + @hangman.incorrect))
+      guess = ''
+      until guess.length == 1
+      	guess = get_input(unguessed_chars)
+      end
+      
+      @hangman.make_guess(guess)
+      turns -= 1
+    end
+    victory?
+    puts hangman.hint
+    puts "You lost! The word was #{hangman.word}"
+  end
+
+  def array_to_regex(array)
+  	regex = array.join('|')
+  	/#{regex}/
+  end
+
+  def victory?
+  	if @hangman.game_over?
+      	puts "You win!"
+ 		exit
+    end
+  end
+
+  def save_game
+  	puts "What do you want to call your file?"
+  	name = ''
+  	while name.length < 1
+  	  print 'Filename:'
+  	  name = gets.chomp
+  	end
+	File.open("saves/#{name}.yaml", 'w') { |file| file.write @hangman.to_yaml }
+	puts "#{name} saved!"
+  	exit
+  end
 end
 
 PlayerInterface.new
