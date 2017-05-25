d161912d0a81de79c38032b56b9c23c8b9141161
Clayton <cjsweeten101@gmail.com>
2017-05-25 14:15:12 -0600

    binary tree creation on unsorted array

--- binary_tree/binarytree.rb
+++ binary_tree/binarytree.rb
@@ -2,20 +2,59 @@ class BinaryTree
   require_relative 'node'
 
   def initialize arr
-  	@tree = build_tree(arr)
+  	@root = nil
+  	build_tree_unsorted(arr)
   end
 
-  def build_tree arr
-  	if arr.length <= 1
+  def build_tree_sorted arr
+  	if arr.length == 1
   	  return Node.new(arr[0])
-  	end
+  	elsif arr.length < 1
+  	  return 
+  	end	
   	midpoint = arr.length/2
   	node = Node.new(arr[midpoint])
-  	node.left = build_tree(arr[0..midpoint - 1])
-  	node.right = build_tree(arr[midpoint + 1.. arr.length])
+  	node.left = build_tree_sorted(arr[0..midpoint - 1])
+  	node.right = build_tree_sorted(arr[midpoint + 1.. arr.length])
   	return node
   end
 
+  def build_tree_unsorted arr
+  	arr.shuffle!
+  	arr.each do |item|
+  	  add_node(Node.new(item))
+  	end
+  	
+  end
+
+  def add_node node
+  	if @root.nil?
+  	  @root= node
+  	  return
+  	end
+    @root = insert_node(@root, node)
+  end
+
+  def insert_node(tree, node)
+    if tree.left.nil? && tree.value > node.value
+      tree.left = node
+      node.parent = tree
+      return get_root(node)
+    elsif tree.right.nil? && tree.value < node.value 
+      tree.right = node
+      node.parent = tree
+      return get_root(node)
+    end
+    tree.value > node.value ? insert_node(tree.left, node) : insert_node(tree.right, node)
+  end
+
+  def get_root(node)
+    if node.parent.nil?
+      return node
+    end
+    get_root(node.parent)
+  end
+  #Depth-First-Traversal technically. . .
   def format tree
   	return if tree.nil?
   	puts tree.value
@@ -24,9 +63,9 @@ class BinaryTree
   end
 
   def to_s 
-  	format @tree
+  	format @root
   end
 end
 
-tree = BinaryTree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-puts tree.to_s
+tree = BinaryTree.new([1, 11, 4, 23, 8, 9, 10, 3, 5, 7, 2, 67, 6345, 324])
+tree.to_s
