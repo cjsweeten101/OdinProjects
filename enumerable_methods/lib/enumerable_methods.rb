331d241c59cf94eaefaeedf39ca47a1285b986e9
Clayton <cjsweeten101@gmail.com>
2017-07-03 12:03:13 -0600

    Finished enumerable methods

--- enumerable_methods/lib/enumerable_methods.rb
+++ enumerable_methods/lib/enumerable_methods.rb
@@ -40,6 +40,64 @@ module Enumerable
   end
 
   def my_any?
-    
+    result = false
+    if block_given?
+      self.my_each do |v|
+        result = true if (yield v)
+      end
+    else
+      self.my_each do |v|
+        result = true if v
+      end
+    end
+    result
   end
+
+  def my_none?
+    result = true
+    if block_given?
+      self.my_each {|v| result = false if (yield v)}
+    else
+      self.my_each {|v| result = false if (v)}
+    end
+    result
+  end
+
+  def my_count *args
+    count = 0
+    if args.empty? && !block_given?
+      self.my_each { count += 1 }
+    elsif block_given?
+      self.my_each {|v| count += 1 if (yield v)}
+    else
+      self.my_each do |v|
+        args.each do |arg|
+          count += 1 if arg == v
+        end
+      end
+    end
+    count
+  end
+
+  def my_map proc = nil
+    result = []
+    return to_enum(:my_select) unless block_given? || proc
+    if proc
+      self.my_each {|v| result << proc.call(v)}
+    else
+      self.my_each {|v| result << (yield v)}
+    end
+    result
+  end
+
+  def my_inject *args
+    args.empty? ? i = 0 : i = args[0]
+    total = self.to_a[i]
+    self.to_a[i+1..self.to_a.length].my_each { |i| total = yield(total,i)}
+    total
+  end
+end
+
+def multiply_els arr
+  arr.my_inject {|tot , i| tot*i}
 end
