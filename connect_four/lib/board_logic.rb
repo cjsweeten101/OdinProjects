415347d201ffb9d5b0a42c085d3bac619e2123c7
Clayton <cjsweeten101@gmail.com>
2017-08-08 14:58:18 -0600

    working on win conditions, column and diagonal four in a row to be worked out

--- connect_four/lib/board_logic.rb
+++ connect_four/lib/board_logic.rb
@@ -20,12 +20,46 @@ class Board
   end
 
   def place(char, col)
-    @status.each_with_index do |row , i|
-      if !row[col-1].nil?
-        @status[i-1][col-1] = char
-      elsif i == @rows - 1
-        @status[6][col-1] = char
+    6.times do |i|
+      if @status[6 - i][col - 1].nil?
+        @status[6 - i][col - 1] = char
+        return true
+      end 
+    end
+    false
+  end
+
+  def four_in_row?
+    cols_count = 0
+    diag_count = 0 
+    rows_count = 0
+
+    @status.each_with_index do |row, i|
+      rows_count = 0
+      current_char = ''
+      next_char = ''
+
+      row.each_with_index do |col, j|
+        if !col.nil?
+          current_char = col
+          next_row_char = @status[i][j + 1]
+
+          if i < @status.length - 1
+            next_col_char = @status[i + 1][j]
+          else
+            next_col_char = nil
+          end
+
+          rows_count += 1 if next_row_char == current_char
+          cols_count +=1 if next_col_char == current_char
+        end
       end
+    end
+
+    if cols_count == 3 || rows_count == 3 || diag_count == 3
+      return true
+    else
+      return false
     end
   end
 end
--- connect_four/spec/board_logic_spec.rb
+++ connect_four/spec/board_logic_spec.rb
@@ -2,41 +2,71 @@ require_relative "../lib/board_logic.rb"
 
 describe "The connect four game board" do 
 
-	before do 
-		@board = Board.new
-	end
+	let!(:board) {Board.new}
 
 	describe "#status" do 
 		it "returns the current status as an array" do 
-			expect(@board.status).to be_a(Array)
+			expect(board.status).to be_a(Array)
 		end
 	end
 
 	describe "#show" do
 		it "returns the current status of the board as a string" do 
-			expect(@board.show).to be_a(String)
+			expect(board.show).to be_a(String)
 		end
 	end
 
 	describe "#place" do 
 		before do 
-			@board.place('X', 1)
-			@board.place('O', 1)
+			board.place('X', 1)
+			board.place('O', 1)
+			board.place('O', 1)
 		end
 
 		it "takes a char as a parameter and places in the designated col, stacking if needed" do
-			expect(@board.status[5][0]).to eq('O')
+			expect(board.status[5][0]).to eq('O')
 		end 
 	end
 
 	describe "#four_in_row?" do 
-		it "checks for four in a row on columns"
 
-		it "checks for four in a row on rows"
+		it "checks for four in a row on columns" do 
+			4.times do 
+				board.place('X', 1)
+			end
+			expect(board.four_in_row?).to eq(true)
+		end
 
-		it "checks for four in a row on diagonals"
+
+		it "checks for four in a row on rows" do 
+			4.times do |i| 
+				board.place('O', i + 1)
+			end
+			expect(board.four_in_row?).to eq(true)
+		end
+
+		it "checks for four in a row on diagonals" do 
+			3.times do |i|
+				board.place('O', 1)
+			end
+
+			2.times do |i|
+				board.place('O', 2) 
+			end
+
+			board.place('O', 3)
+
+			board.place('X', 1)
+			board.place('X', 2)
+			board.place('X', 3)
+			board.place('X', 4)
+
+			puts board.show
+
+			expect(board.four_in_row?).to eq(true)
+		end
+
 	end
-
 	describe "#full?" do 
 		it "checks if the whole board is full, resulting in a tie"
 	end
