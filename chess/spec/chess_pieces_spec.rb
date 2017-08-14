7ec0af3312af1f06f81efa4f69229df8255eab6e
Clayton <cjsweeten101@gmail.com>
2017-08-14 15:10:27 -0600

    Add basic tests for generall pieces/knight

---
+++ chess/spec/chess_pieces_spec.rb
@@ -0,0 +1,25 @@
+require_relative "../lib/chess_pieces.rb"
+
+describe "A Chess piece" do
+		before do 
+			@piece = Piece.new 'w'
+		end
+
+	it "can be white or black" do 
+		expect(@piece.color).to eq("w")
+	end
+
+	describe "A Knight" do 
+		before do 
+			@knight = Knight.new
+		end
+
+		it "can move in an 'L' shape" do 
+			expect(@knight.moveset).to include([2,1])
+		end
+
+		it "cannot move in a straight line" do 
+			expect(@knight.moveset).not_to include([2,0])
+		end
+	end
+end
